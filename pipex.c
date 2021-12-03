a09492cbad8b6f5411bda938142b002cd57422b1
Runnerlizard <smolev_sergey@mail.ru>
2021-12-03 12:25:16 +0300

    close for dup2 fixed. need free and another closes

--- pipex.c
+++ pipex.c
@@ -45,29 +45,21 @@ int	main(int argc, char *argv[], char **env)
 	i = 0;
 	if (!pipe(p->fd))
 	{
-		ft_printf("1\n");
 		p->pid = fork();
 		if (p->pid == 0)
 		{
-			ft_printf("2\n");
+			close(p->fd[0]);
 			dup2(p->file1, STDIN_FILENO);
-			ft_printf("3\n");
 			dup2(p->fd[1], STDOUT_FILENO);
-			ft_printf("4\n");
 			execve(ft_strjoin("/usr/bin/", p->cmd[0][0]), p->cmd[0], env);
-			ft_putstr_fd("didnt happened\n", 1);
 		}
 		else if (p->pid > 0)
 		{
-			ft_printf("5\n");
 			wait(NULL);
-			ft_printf("6\n");
+			close(p->fd[1]);
 			dup2(p->fd[0], STDIN_FILENO);
-			ft_printf("7\n");
 			dup2(p->file2, STDOUT_FILENO);
-			ft_printf("8\n");
 			execve(ft_strjoin("/usr/bin/", p->cmd[1][0]), p->cmd[1], env);
-			ft_putstr_fd("\nDone\n", 1);
 		}
 		else
 			return (ft_printf("fork: %s\n", strerror(errno)));
@@ -75,51 +67,4 @@ int	main(int argc, char *argv[], char **env)
 	else
 		return(ft_printf("pipe: %s\n", strerror(errno)));
 	return (i);
-
-
-	/*
-		if (!pipe(fd))
-	{	
-		id = fork();
-		if (id == -1)
-		{
-			perror ("fork");
-			return (1);
-		}
-		if (id == 0)
-		{
-			if (close(fd[0]))
-				perror("close\n");
-			x = "From child";
-			if (write(fd[1], &x, sizeof(x)) == -1)
-			{
-				perror("write\n");
-				return (1);
-			}
-			if (close(fd[1]))
-				perror("close\n");
-		}
-		else
-		{
-			if (close(fd[1]))
-				perror("close\n");
-			if (read(fd[0], &y, sizeof(y)) == -1)
-			{
-				perror("read\n");
-				return (1);
-			}
-			if (close(fd[0]))
-				perror("close\n");
-			printf("Got %s\n", y);
-		}
-	}
-	else
-	{
-		perror ("pipe");
-		return (1);
-	}
-	
-	while (!(wait(0)) || (errno != ECHILD))
-		;
-	free(newargv);*/
 }
