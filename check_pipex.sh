4c1df7313c8167873323924669db26536a2ac55e
runnerlizard <smolev_sergey@mail.ru>
2022-01-15 11:03:07 +0300

    path detector added

--- check_pipex.sh
+++ check_pipex.sh
@@ -12,15 +12,15 @@ rm -rf infile || true
 rm -rf outfile || true
 
 ./pipex infile "ls -l" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
 
 < infile ls -l | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 1 - \e[1;32mOK\n\e[0m'
@@ -46,8 +46,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 ./pipex infile "ls -l" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -59,8 +59,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 < infile ls -l | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 2 - \e[1;32mOK\n\e[0m'
@@ -86,8 +86,8 @@ touch infile
 chmod 777 infile
 
 ./pipex infile "ls -l" "wc" outf
-stat --format="%a" outf >>./temporary/res1
-cat outf >>./temporary/res1
+stat --format="%a" outf >>./temporary/res1 2>/dev/null
+cat outf >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outf || true
@@ -96,8 +96,8 @@ touch infile
 chmod 777 infile
 
 < infile ls -l | wc > outf
-stat --format="%a" outf >>./temporary/res2
-cat outf >>./temporary/res2
+stat --format="%a" outf >>./temporary/res2 2>/dev/null
+cat outf >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 3 - \e[1;32mOK\n\e[0m'
@@ -121,8 +121,8 @@ chmod 777 outfile
 echo blablabla >outfile
 
 ./pipex infile "ls -l" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -132,8 +132,8 @@ chmod 777 outfile
 echo blablabla >outfile
 
 < infile ls -l | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 4 - \e[1;32mOK\n\e[0m'
@@ -159,7 +159,7 @@ chmod 000 outfile
 echo blablabla >infile
 
 ./pipex infile "ls -l" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
 cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
@@ -171,7 +171,7 @@ chmod 000 outfile
 echo blablabla >infile
 
 < infile ls -l | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
 cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
@@ -197,8 +197,8 @@ chmod 777 outfile
 echo blablabla >outfile
 
 ./pipex infile "ls -l" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -209,8 +209,8 @@ chmod 777 outfile
 echo blablabla >outfile
 
 < infile ls -l | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 6 - \e[1;32mOK\n\e[0m'
@@ -236,8 +236,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 ./pipex infile "ls -l -a" "wc -l -c" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -249,8 +249,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 < infile ls -l -a | wc -l -c > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 7 - \e[1;32mOK\n\e[0m'
@@ -277,8 +277,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 ./pipex infile "ls -l" "blabla" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -290,8 +290,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 < infile ls -l | blabla > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 8 - \e[1;32mOK\n\e[0m'
@@ -317,8 +317,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 ./pipex infile "blablabla" "ls -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -330,8 +330,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 < infile blablabla | ls -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 9 - \e[1;32mOK\n\e[0m'
@@ -358,8 +358,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 ./pipex infile "blablabla" "kljjsdlfh" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -371,8 +371,8 @@ echo blablabla >infile
 echo blablabla >outfile
 
 < infile blablabla | kljjsdlfh > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 10 - \e[1;32mOK\n\e[0m'
@@ -400,8 +400,8 @@ chmod 777 infile
 chmod 000 outfile
 
 ./pipex infile "lgfergerg" "wc -l" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1 2>/dev/null
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -411,8 +411,8 @@ chmod 777 infile
 chmod 000 outfile
 
 < infile lgfergerg | wc -l > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2 2>/dev/null
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 11 - \e[1;32mOK\n\e[0m'
@@ -440,8 +440,8 @@ chmod 777 infile
 chmod 000 outfile
 
 ./pipex infile "lgfergerg" "tykjyj" outfile
-stat --format="%a" outfile >./temporary/res1
-cat outfile >>./temporary/res1 2>/dev/null
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null 2>/dev/null
 
 rm -rf infile || true
 rm -rf outfile || true
@@ -451,8 +451,8 @@ chmod 777 infile
 chmod 000 outfile
 
 < infile lgfergerg | tykjyj > outfile
-stat --format="%a" outfile >./temporary/res2
-cat outfile >>./temporary/res2 2>/dev/null
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
     printf 'Test 12 - \e[1;32mOK\n\e[0m'
@@ -478,7 +478,7 @@ touch infile outfile
 chmod 000 infile
 
 ./pipex infile "ls -la" "wc" outfile
-stat --format="%a" outfile >./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
 cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
@@ -488,7 +488,7 @@ touch infile outfile
 chmod 000 infile
 
 < infile ls -la| wc > outfile
-stat --format="%a" outfile >./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
 cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
@@ -516,7 +516,7 @@ touch infile outfile
 chmod 000 infile
 
 ./pipex infile "hdht" "wc" outfile
-stat --format="%a" outfile >./temporary/res1
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
 cat outfile >>./temporary/res1 2>/dev/null
 
 rm -rf infile || true
@@ -526,7 +526,7 @@ touch infile outfile
 chmod 000 infile
 
 < infile hdht | wc > outfile
-stat --format="%a" outfile >./temporary/res2
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
 cat outfile >>./temporary/res2 2>/dev/null
 
 if cmp -s "./temporary/res1" "./temporary/res2"; then
@@ -541,16 +541,96 @@ rm -rf temporary || true
 
 
 
+#15==================df grep
+mkdir -p temporary
+cd temporary
+touch res1 res2
+cd ..
+
+touch infile outfile
+chmod 777 infile
+chmod 777 outfile
+echo blablabla >infile
+echo blablabla >outfile
+
+./pipex infile "df" "grep udev" outfile
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
+
+rm -rf infile || true
+rm -rf outfile || true
+
+touch infile outfile
+chmod 777 infile
+chmod 777 outfile
+echo blablabla >infile
+echo blablabla >outfile
+
+< infile df | grep udev > outfile
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
+
+if cmp -s "./temporary/res1" "./temporary/res2"; then
+    printf 'Test 15 - \e[1;32mOK\n\e[0m'
+else
+    printf 'Test 15 df grep - \e[1;31mKO\n\e[0m'
+fi
+
+rm -rf infile || true
+rm -rf outfile || true
+rm -rf temporary || true
+
+
+
+#15==================df grep
+mkdir -p temporary
+cd temporary
+touch res1 res2
+cd ..
+
+touch infile outfile
+chmod 777 infile
+chmod 777 outfile
+echo blablabla >infile
+echo blablabla >outfile
+
+./pipex infile "/usr/bin/df" "grep udev" outfile
+stat --format="%a" outfile >./temporary/res1 2>/dev/null
+cat outfile >>./temporary/res1 2>/dev/null
+
+rm -rf infile || true
+rm -rf outfile || true
+
+touch infile outfile
+chmod 777 infile
+chmod 777 outfile
+echo blablabla >infile
+echo blablabla >outfile
+
+< infile /usr/bin/df | grep udev > outfile
+stat --format="%a" outfile >./temporary/res2 2>/dev/null
+cat outfile >>./temporary/res2 2>/dev/null
+
+if cmp -s "./temporary/res1" "./temporary/res2"; then
+    printf 'Test 16 - \e[1;32mOK\n\e[0m'
+else
+    printf 'Test 16 /usr/bin/df grep - \e[1;31mKO\n\e[0m'
+fi
+
+rm -rf infile || true
+rm -rf outfile || true
+rm -rf temporary || true
+
+
 
 #################   VALGRIND  ############################
 
 #1 
 
-rm -rf infile || true
-rm -rf outfile || true
+touch infile outfile
 
-valgrind --leak-check=full --track-origins=yes --log-file="logValgrind" ./pipex infile "ls -l" "wc -l" outfile || true
-if (<logValgrind wc -l) > 15
-echo KO
+valgrind --leak-check=full --log-file="logValgrind" ./pipex infile "ls -l" "wc -l" outfile 
+#if (<logValgrind wc -l) > 15
+#echo KO
 
 rm -rf logValgrind || true
--- pipex.c
+++ pipex.c
@@ -54,12 +54,62 @@ static void	close_and_free(char *s, t_pipex *p, char *message, int cmd)
 	{
 		if ((s[i] == '1') || (s[i] == '3') || (s[i] == '4'))
 			free_eto(s[i], p);
+		else if (s[i] == '2')
+			unlink("clucotestfile");
 		else if (s[i] == '5')
 			close(p->file1);
 		else if (s[i] == '6')
 			close(p->file2);
 	}
 	exit(1);
+}
+
+int get_next_line(char **line, int fd)
+{
+	int rd;
+	int i;
+	char ch;
+	char *buffer;
+
+	rd = 0;
+	i = 0;
+	buffer = malloc(100);
+	*line = buffer;
+	while ((rd = read(fd, &ch, 1)) > 0 && ch != '\n')
+		buffer[i++] = ch;
+	buffer[i] = '\0';
+	return (rd);
+}
+
+static char	*get_path(char *str, t_pipex *p, char **env)
+{
+	int		tmp_fd;
+	pid_t	pid;
+	char	*args[3];
+	char	*path;
+
+	tmp_fd = open("clucotestfile", O_TRUNC | O_CREAT | O_RDWR, 0664);
+	pid = fork();
+	if (pid == 0)
+	{
+		args[0] = "which";
+		args[1] = str;
+		args[2] = NULL;
+		if (dup2(tmp_fd, STDOUT_FILENO) < 0)
+			perror("dup2");
+		execve("/usr/bin/which", args, env);
+	}
+	else if (pid > 0)
+	{
+		tmp_fd = open("clucotestfile", O_RDONLY);
+		if (wait(NULL) < 0)
+			perror("wait");
+		get_next_line(&path, tmp_fd);
+		free(str);
+		return (path);
+	}
+	close_and_free("3412", p, "get_path", 0);
+	return (NULL);
 }
 
 static void cmd1(t_pipex *p, char **env, char **argv)
@@ -72,7 +122,7 @@ static void cmd1(t_pipex *p, char **env, char **argv)
 	if ((dup2(p->file1, STDIN_FILENO) < 0) || (dup2(p->fd[1], STDOUT_FILENO) < 0))
 		perror("dup2");
 	p->file2 = open(argv[4], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	execve(ft_strjoin("/usr/bin/", p->cmd[0][0]), p->cmd[0], env);
+	execve(p->cmd[0][0], p->cmd[0], env);
 	close_and_free("3415", p, argv[2], 1);
 }
 
@@ -87,7 +137,7 @@ static void cmd2(t_pipex *p, char **env, char **argv)
 		perror("close");
 	if ((dup2(p->fd[0], STDIN_FILENO) < 0) || (dup2(p->file2, STDOUT_FILENO) < 0))
 		perror("dup2");
-	execve(ft_strjoin("/usr/bin/", p->cmd[1][0]), p->cmd[1], env);
+	execve(p->cmd[1][0], p->cmd[1], env);
 	close_and_free("6341", p, argv[3], 1);
 }
 	
@@ -106,9 +156,12 @@ int	main(int argc, char *argv[], char **env)
 	while (++i < argc - 3)
 		if (!(p->cmd[i] = ft_split(argv[2 + i], ' ')))
 			close_and_free("41", p, "malloc p->cmd[i]", 0);
+	p->cmd[0][0] = get_path(p->cmd[0][0], p, env);
+	p->cmd[1][0] = get_path(p->cmd[1][0], p, env);
 	if (pipe(p->fd))
-		close_and_free("341", p, "pipe", 0);
+		close_and_free("3412", p, "pipe", 0);
 	p->pid1 = fork();
+	unlink("clucotestfile");
 	if (p->pid1 == 0)
 		cmd1(p, env, argv);
 	else if (p->pid1 > 0)
